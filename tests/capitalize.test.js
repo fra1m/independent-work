352b2c043cdc184ce8910fd4f95a1f3d082b7001
vasili <frugas@list.ru>
2022-11-29 15:17:50 +0300

    used assert.equal

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,8 @@
+import { strict as assert } from 'node:assert';
+
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-  }
+assert.equal(capitalize(''), '')
+assert.equal(capitalize('hello'), 'Hello')
   
-  if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-  }
-  
-  console.log('Все тесты пройдены!');
+console.log('Все тесты пройдены!');
